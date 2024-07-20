eff2466e2d98374996a787497d1d9a58e1894d56
thoma <73348376+tom-devv@users.noreply.github.com>
2024-07-20 12:40:22 +0100

    remove unused

--- src/client/java/dev/tom/looper/client/LooperClient.java
+++ src/client/java/dev/tom/looper/client/LooperClient.java
@@ -5,6 +5,7 @@ import com.mojang.brigadier.arguments.StringArgumentType;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents;
 
 
 public class LooperClient implements ClientModInitializer {
@@ -29,22 +30,5 @@ public class LooperClient implements ClientModInitializer {
             );
 
         });
-//        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
-//            dispatcher.register(ClientCommandManager.literal("loop")
-//                    .then(CommandManager.argument("number", IntegerArgumentType.integer())
-//                            .then(CommandManager.argument("command", StringArgumentType.greedyString())
-//                                    .executes(context -> {
-//                                        int count = IntegerArgumentType.getInteger(context, "number");
-//                                        String command = StringArgumentType.getString(context, "command");
-//                                        CommandDispatcher<ServerCommandSource> commandDispatcher = context.getSource().getServer().getCommandManager().getDispatcher();
-//                                        for (int i = 0; i < count; i++) {
-//                                            commandDispatcher.execute(command, context.getSource());
-//                                        }
-//                                        return 1;
-//                                    })
-//                            )
-//                    )
-//            );
-//        });
     }
 }
--- src/main/java/dev/tom/looper/Looper.java
+++ src/main/java/dev/tom/looper/Looper.java
@@ -1,17 +1,6 @@
 package dev.tom.looper;
 
-import com.mojang.authlib.minecraft.client.MinecraftClient;
-import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.arguments.IntegerArgumentType;
-import com.mojang.brigadier.arguments.StringArgumentType;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.network.message.SentMessage;
-import net.minecraft.server.command.CommandManager;
-import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.Text;
 
 public class Looper implements ModInitializer {
 
